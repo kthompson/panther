3171c23d64d52566aa751a6d644eb9beddb99276
Kevin Thompson <kevin@theautomaters.com>
2020-09-02 20:04:47 -0700

    Reorg some of the StdLib and add println overload

--- src/Panther.StdLib/Predef.cs
+++ src/Panther.StdLib/Predef.cs
@@ -6,8 +6,8 @@ namespace Panther
     {
         public static void print(object value) => Console.Write(value);
         public static void println(object value) => Console.WriteLine(value);
-
-        public static string readLine() => Console.ReadLine();
+        public static void println() => Console.WriteLine();
+        public static string? readLine() => Console.ReadLine();
 
         private static Random _random = new Random();
         public static int rnd(int max) => _random.Next(max);
