6c94fd26521b2463939e33be38d34e2dc39aef78
Kevin Thompson <kevin.thompson@agrian.com>
2021-08-13 20:31:56 -0700

    Update Nuke. use Nuke.Components

---
+++ build/Components/IHazGitRepository.cs
@@ -0,0 +1,11 @@
+﻿using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.ValueInjection;
+
+namespace Panther.Build.Components
+{
+    public interface IHazGitRepository : INukeBuild
+    {
+        [GitRepository] [Required] GitRepository GitRepository => ValueInjectionUtility.TryGetValue(() => GitRepository);
+    }
+}
