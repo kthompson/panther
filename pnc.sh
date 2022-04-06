73a23519b9d15edd75c99af60cdec972ec07d670
Kevin Thompson <kevin@theautomaters.com>
2022-04-05 20:38:10 -0700

    Fix CLI scripts

--- pnc.sh
+++ pnc.sh
@@ -4,8 +4,8 @@
 slndir="$(dirname "${BASH_SOURCE[0]}")/src"
 
 # Restore + Build
-dotnet build "$slndir/Panther.Compiler" --nologo || exit
+dotnet build "$slndir/Panther" --nologo || exit
 dotnet build "$slndir/Panther.StdLib" --nologo || exit
 
 # Run
-dotnet run -p "$slndir/Panther.Compiler" --no-build -- "$@"
+dotnet run -p "$slndir/Panther" --no-build -- "$@"
