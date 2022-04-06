8df272035f0dc983ffc69268cc2c32e75c06c3dc
Kevin Thompson <kevin@theautomaters.com>
2022-04-05 20:38:10 -0700

    Fix CLI scripts

--- pnc.cmd
+++ pnc.cmd
@@ -4,8 +4,8 @@ REM Vars
 set "SLNDIR=%~dp0src"
 
 REM Restore + Build
-dotnet build "%SLNDIR%\Panther.Compiler" --nologo || exit /b
+dotnet build "%SLNDIR%\Panther" --nologo || exit /b
 dotnet build "%SLNDIR%\Panther.StdLib" --nologo || exit /b
 
 REM Run
-dotnet run -p "%SLNDIR%\Panther.Compiler" --no-build -- %*
+dotnet run -p "%SLNDIR%\Panther" --no-build -- %*
--- src/Panther/Program.cs
+++ src/Panther/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Panther;
 using Panther.CodeAnalysis;
 using Panther.CodeAnalysis.Syntax;
 using Panther.IO;
