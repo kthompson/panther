eed10532e0ae08ba65a846bd6a1223d00d5bb78c
Kevin Thompson <kevin@theautomaters.com>
2022-09-06 20:56:53 -0700

    Improve debug output

--- src/Panther/CodeAnalysis/Lowering/LoweringPipeline.cs
+++ src/Panther/CodeAnalysis/Lowering/LoweringPipeline.cs
@@ -12,49 +12,70 @@ internal sealed class LoweringPipeline
         if (debug)
         {
             Console.WriteLine("==== Original Code ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             statement.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var boundStatement = LoopLowerer.Lower(method, statement);
         if (debug)
         {
             Console.WriteLine("==== Lowered Code ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             boundStatement.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var noIndexExpr = IndexExpressions.Lower(method, boundStatement);
         if (debug)
         {
             Console.WriteLine("==== Index Expressions ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             noIndexExpr.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var tac = ThreeAddressCode.Lower(method, noIndexExpr);
         if (debug)
         {
             Console.WriteLine("==== Three Address Code ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             tac.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var unitLessStatements = RemoveUnitAssignments.Lower(tac);
         if (debug)
         {
             Console.WriteLine("==== Remove Unit Assignments ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             unitLessStatements.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var inlinedTemporaries = InlineTemporaries.Lower(method, unitLessStatements);
         if (debug)
         {
             Console.WriteLine("==== Inlined Temporaries ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             inlinedTemporaries.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         var deadCodeRemoval = DeadCodeRemoval.RemoveDeadCode(inlinedTemporaries);
         if (debug)
         {
             Console.WriteLine("==== Dead Code Removal ===");
+            method.WriteTo(Console.Out);
+            Console.Out.Write(" = ");
             deadCodeRemoval.WriteTo(Console.Out);
+            Console.Out.WriteLine();
         }
 
         return deadCodeRemoval;
